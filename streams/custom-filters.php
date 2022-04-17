664e30c7079e1e3be076d04640ffd2dc0eb5f888
Tom Wilford <hello@jollyblueman.com>
2022-04-17 17:02:36 +0100

    added custom stream wrapper

---
+++ streams/custom-filters.php
@@ -0,0 +1,19 @@
+<?php
+
+include './DirtyWordsFilter.php';
+
+stream_filter_register('string.dirty_words_filter', 'DirtyWordsFilter');
+
+$handle = fopen('./test/example.csv', 'r+');
+stream_filter_append($handle, 'string.dirty_words_filter');
+while (feof($handle) !== true) {
+    echo fgets($handle);
+}
+echo PHP_EOL;
+echo PHP_EOL;
+
+$handle = fopen("php://filter/read=string.dirty_words_filter/resource=test/example.csv", 'rb');
+while (feof($handle) !== true) {
+    echo fgets($handle);
+}
+echo PHP_EOL;
